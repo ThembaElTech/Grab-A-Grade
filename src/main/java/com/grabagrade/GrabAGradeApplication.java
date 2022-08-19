8e837286ac7bebe2a5cb0f78702d79e9183a3eb9
Emenice Mhungu <emenice@Emenices-MacBook-Pro.local>
2022-08-19 15:44:22 +0200

    initializing spring boot project

---
+++ src/main/java/com/grabagrade/GrabAGradeApplication.java
@@ -0,0 +1,13 @@
+package com.grabagrade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrabAGradeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrabAGradeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grabagrade/GrabeAGradeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grabagrade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrabeAGradeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
