64113bde85d0321adaa14e6bcf6297e3519c23d4
BennedictMoraba <mulisa648@gmail.com>
2022-09-14 14:14:47 +0200

    It was agreed that we delete every code we had from before so that we can safely incorporate the new UIs with their respective Interface, Controllers and models. Deleted Items -All html pages -All css under templates -All controllers and interfaces -All models
    Added Items
    -static folder
     *css and images folder
      $css/signUp.css
      $images/ social icons
    
     under "com.grabagrade" the following were added
     Folder
      -controller
       $ SignUpImpl
      -model
      -interfaces
       $ ISignUp
      -services

---
+++ src/main/java/com/grabagrade/controller/SIgnUpImpl.java
@@ -0,0 +1,14 @@
+package com.grabagrade.controller;
+
+import com.grabagrade.interfaces.ISignUp;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SIgnUpImpl implements ISignUp {
+    @Override
+    @GetMapping("/signup")
+    public String showSignUp() {
+        return "signUp";
+    }
+}
