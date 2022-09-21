81364e2798688d8def32a32ed318319daf18d38c
BennedictMoraba <mulisa648@gmail.com>
2022-09-21 07:25:45 +0200

    Changes made: -Firebase Initialization in the Grab-A-Grade main Application -Added firebase-admin in the pom.xml file -Added a serviceAccountKey.json under resource

--- src/main/java/com/grabagrade/GrabAGradeApplication.java
+++ src/main/java/com/grabagrade/GrabAGradeApplication.java
@@ -1,12 +1,41 @@
 package com.grabagrade;
 
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Objects;
 
 @SpringBootApplication
 public class GrabAGradeApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException
+	{
+		ClassLoader classLoader = GrabAGradeApplication.class.getClassLoader();
+		File file = new File(Objects.requireNonNull(classLoader.getResource("./serviceAccountKey.json")).getFile());
+
+
+		FileInputStream serviceAccount =
+				new FileInputStream(file.getAbsolutePath());
+
+		FirebaseOptions options = new FirebaseOptions.Builder()
+				.setCredentials(GoogleCredentials.fromStream(serviceAccount))
+				.setDatabaseUrl("https://grab-a-grade-98b82-default-rtdb.firebaseio.com")
+				.build();
+
+
+		if(FirebaseApp.getApps().isEmpty())
+		{
+			FirebaseApp.initializeApp(options);
+		}
+
+
+
 		SpringApplication.run(GrabAGradeApplication.class, args);
 	}
 
